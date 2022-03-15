f5112a53e00e86f17aa29d9372d3fdd0020832d5
André Gomes <48630053+aandregomes@users.noreply.github.com>
2022-03-14 21:12:58 -0300

    Criando pomodoro timer 01 (#1)
    * criando-pomodoro-timer-01 -> first commit
    
    * criando-pomodoro-timer-01 -> iniciando com testes
    
    * pomodoro-timer -> começando o serializer

---
+++ pomodoro/core/tests/test_views.py
@@ -0,0 +1,16 @@
+def test_request_criar_nova_tarefa(client):
+    resp = client.post('/api/tasks', {
+        "description": "Criando task de teste"
+    }, content_type="application/json")
+
+    assert resp.status_code == 201
+
+
+def test_response_criar_nova_tarefa(client):
+    resp = client.post('/api/tasks', {
+        "description": "Criando task de teste"
+    }, content_type="application/json")
+
+    assert "uid" in resp.data
+    assert "created_at" in resp.data
+    assert resp.data["description"] == "Criando task de teste"
