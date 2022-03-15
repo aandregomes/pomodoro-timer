074fa6af5a2bed978d1060ff892d44e062dd6f3f
Andre Gomes <andre.gms94@gmail.com>
2022-03-14 21:06:00 -0300

    pomodoro-timer -> começando o serializer

---
+++ pomodoro/core/tests/test_serializer.py
@@ -0,0 +1,11 @@
+from pomodoro.core.serializers import TaskSerializer
+
+
+def test_task_serializer():
+    task = {
+        "uid": "f560489a-bc57-4f55-b236-c30af43b3b01",
+        "description": "Task teste",
+        "created_at": "2022-03-14T20:55:00"
+    }
+    serializer = TaskSerializer(data=task)
+    assert serializer.initial_data == task
